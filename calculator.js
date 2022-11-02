9bbbe41566aa8ffd2ab0a3caf410a1f9264aa474
Austin Miller <austinmillerwebdev@gmail.com>
2022-11-01 22:00:26 -0700

    Add basic arithmetic functions to JS

--- calculator.js
+++ calculator.js
@@ -1,5 +1,25 @@
 let calc = {};
 
+calc.add = (a, b) => a + b;
+calc.subtract = (a, b) => a - b;
+calc.multiply = (a, b) => a * b;
+calc.divide = (a, b) => a / b;
+
+calc.operate = (a, b, operator) => {
+  switch (operator) {
+    case '+':
+      return calc.add(a, b);
+    case '-':
+      return calc.subtract(a, b);
+    case '*':
+      return calc.multiply(a, b);
+    case '/':
+      return calc.divide(a, b);
+    default:
+      return 'ERROR';
+  }
+}
+
 calc.init = () => {
   //do stuff
 }
